c228d63247993745f02d0fa7f074979e14c50ebe
myroslavsuprun <myroslavsuprun@gmail.com>
2024-04-20 23:02:28 +0300

    add Add function comments

--- pbadd.go
+++ pbadd.go
@@ -1,5 +1,10 @@
 package pbadd
 
+// Add sums two numbers and returns the result.
+//
+// A reference [link].
+//
+// [link]: https://www.mathsisfun.com/numbers/addition.html
 func Add(f, s int) int {
 	return f + s
 }
