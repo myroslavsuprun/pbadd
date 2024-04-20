61eaf53b0148732cbaad87ffdb88821f4658afdf
myroslavsuprun <myroslavsuprun@gmail.com>
2024-04-20 23:08:25 +0300

    make Add function generic; add Number type

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/myroslavsuprun/pbadd
 
 go 1.22.1
+
+require golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f
--- pbadd.go
+++ pbadd.go
@@ -1,10 +1,18 @@
 package pbadd
 
+import "golang.org/x/exp/constraints"
+
+// Number interface contains a generic type for the parameners of the following funcitons:
+// [github.com/myroslavsuprun/pbadd.Add]
+type Number interface {
+	constraints.Float | constraints.Integer
+}
+
 // Add sums two numbers and returns the result.
 //
 // A reference [link].
 //
 // [link]: https://www.mathsisfun.com/numbers/addition.html
-func Add(f, s int) int {
+func Add[T Number](f, s T) T {
 	return f + s
 }
